d8665179d43cdf0d9a32302702261f7d3a97179d
Thomas Chambeyron <thomas.chambeyron@outlook.fr>
2024-06-12 14:43:17 -0400

    Ajout de la fonction trapezes en python avec Numpy

--- methodes_numpy.py
+++ methodes_numpy.py
@@ -5,6 +5,7 @@ Ce module fournit plusieurs méthodes pour calculer l'intégrale d'un polynôme 
 
 Fonctions:
     - f: Calcule la valeur du polynôme de degré 3 à un point x donné.
+    - trapezes: Calcule l'intégrale du polynôme de degré 3 sur [a, b] avec  la méthode des trapèzes vectorisée.
 
 Auteurs: Lou-Anne Villette & Thomas Chambeyron
 Date: 05/06/2024
@@ -26,3 +27,22 @@ def f(p, x):
     (float) Valeur du polynôme f(x) = p[0] + p[1] * x + p[2] * x**2 + p[3] * x**3
     """
     return p[0] + p[1] * x + p[2] * x**2 + p[3] * x**3
+
+
+# Méthode des trapèzes
+def trapezes(p, a, b, n=10):
+    """
+    Calcule l'intégrale du polynôme de degré 3 sur [a, b] avec la méthode des trapèzes vectorisée.
+
+    Arguments:
+    p (list) : liste des coefficients du polynôme
+    a, b (float) : bornes de l'intervalle d'intégration
+    n (int) : nombre de segments (10 par défaut)
+
+    Retour:
+    (float) Valeur approximative de l'intégrale de f(x) sur [a, b] par la méthode des trapèzes vectorisée
+    """
+    pas = (b - a) / n
+    x = np.linspace(a, b, n+1)
+    y = f(p, x)
+    return ((y[0] + y[-1])/2 + np.sum(y[1:-1])) * pas
