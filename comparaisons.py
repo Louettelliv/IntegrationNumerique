af0a2afe8ca1f0a987f4341cfe8bae720c143f39
200~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~ <louannevillette2000@gmail.com>
2024-06-12 14:46:41 -0400

    Ajout de la fonction erreur_absolue pour calculer l'erreur absolue entre la valeur analytique et la valeur numérique

--- comparaisons.py
+++ comparaisons.py
@@ -5,6 +5,9 @@ Ce module permet de comparer différentes méthodes d'intégration numérique po
 Les méthodes de comparaison incluent le calcul de l'erreur, l'analyse de la convergence, et la mesure du temps
 d'exécution.
 
+Fonctions:
+    - erreur_absolue: Calcule l'erreur absolue entre l'intégrale analytique et l'intégrale numérique.
+
 Auteurs: Lou-Anne Villette & Thomas Chambeyron
 Date: 05/06/2024
 """
@@ -16,3 +19,18 @@ import methodes_scipy
 from timeit import timeit
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+# Calcul de l'erreur
+def erreur_absolue(analytique, numerique):
+    """
+    Calcule l'erreur absolue entre l'intégrale analytique et l'intégrale numérique.
+
+    Arguments:
+    analytique (float) : valeur de l'intégrale analytique
+    numerique (float) : valeur de l'intégrale numérique
+
+    Retour:
+    (float) Erreur absolue entre les deux valeurs
+    """
+    return abs(analytique - numerique)
