ad2d85461d76d18c3097006435741080abb526e6
200~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~ <louannevillette2000@gmail.com>
2024-06-12 11:03:30 -0400

    Ajout de la fonction rectangles dans methodes_python.py pour intégrer numériquement avec la méthodes des rectangles en python de base

--- methodes_python.py
+++ methodes_python.py
@@ -6,6 +6,7 @@ Ce module fournit plusieurs méthodes pour calculer l'intégrale d'un polynôme 
 Fonctions:
     - f: Calcule la valeur du polynôme de degré 3 à un point x donné.
     - analytique: Calcule l'intégrale analytique du polynôme de degré 3 sur [a, b].
+    - rectangles: Calcule l'intégrale du polynôme de degré 3 sur [a, b] avec la méthode des rectangles.
 
 Auteurs: Lou-Anne Villette & Thomas Chambeyron
 Date: 05/06/2024
@@ -43,3 +44,24 @@ def analytique(p, a, b):
             p[1] * (b**2 - a**2) / 2 +
             p[2] * (b**3 - a**3) / 3 +
             p[3] * (b**4 - a**4) / 4)
+
+
+# Méthode des rectangles
+def rectangles(p, a, b, n=10):
+    """
+    Calcule l'intégrale du polynôme de degré 3 sur [a, b] avec la méthode des rectangles.
+
+    Arguments:
+    p (list) : liste des coefficients du polynôme
+    a, b (float) : bornes de l'intervalle d'intégration
+    n (int) : nombre de segments (10 par défaut)
+
+    Retour:
+    integrale (float) : valeur approximative de l'intégrale de f(x) sur [a, b] par la méthode des rectangles
+    """
+    pas = (b - a) / n
+    integrale = 0
+    for i in range(n):
+        x = a + (i + 0.5) * pas
+        integrale += f(p, x) * pas
+    return integrale
