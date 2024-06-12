c7ba2d4a329cb8ef0009aa410f6874078243e7fa
Thomas Chambeyron <thomas.chambeyron@outlook.fr>
2024-06-12 14:41:12 -0400

    Ajout de la fonction trapezes en python de base

--- methodes_python.py
+++ methodes_python.py
@@ -6,6 +6,8 @@ Ce module fournit plusieurs méthodes pour calculer l'intégrale d'un polynôme 
 Fonctions:
     - f: Calcule la valeur du polynôme de degré 3 à un point x donné.
     - analytique: Calcule l'intégrale analytique du polynôme de degré 3 sur [a, b].
+    - rectangles: Calcule l'intégrale du polynôme de degré 3 sur [a, b] avec la méthode des rectangles.
+    - trapezes: Calcule l'intégrale du polynôme de degré 3 sur [a, b] avec  la méthode des trapèzes.
 
 Auteurs: Lou-Anne Villette & Thomas Chambeyron
 Date: 05/06/2024
@@ -43,3 +45,25 @@ def analytique(p, a, b):
             p[1] * (b**2 - a**2) / 2 +
             p[2] * (b**3 - a**3) / 3 +
             p[3] * (b**4 - a**4) / 4)
+
+
+# Méthode des trapèzes
+def trapezes(p, a, b, n=10):
+    """
+    Calcule l'intégrale du polynôme de degré 3 sur [a, b] avec la méthode des trapèzes.
+
+    Arguments:
+    p (list) : liste des coefficients du polynôme
+    a, b (float) : bornes de l'intervalle d'intégration
+    n (int) : nombre de segments (10 par défaut)
+
+    Retour:
+    integrale (float) : valeur approximative de l'intégrale de f(x) sur [a, b] par la méthode des trapèzes
+    """
+    pas = (b - a) / n
+    integrale = (f(p, a) + f(p, b)) / 2
+    for i in range(1, n):
+        x = a + i * pas
+        integrale += f(p, x)
+    integrale *= pas
+    return integrale
